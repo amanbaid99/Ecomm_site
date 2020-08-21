fce09e294f9a5f646732a5c6c01112939eda5b77
amanbaid99 <amanbaid99@gmail.com>
2020-08-21 11:52:55 +0530

    configured templates and routing

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name='home'),
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('store/',views.store,name='store'),
+
 ]
--- foodapp/views.py
+++ foodapp/views.py
@@ -5,3 +5,16 @@ from django.shortcuts import render
 def home(request):
     return render(request,"foodapp/home.html")
 
+def store(request):
+    return render(request,"foodapp/store.html")
+
+def cart(request):
+    return render(request,"foodapp/cart.html")
+
+def checkout(request):
+    return render(request,"foodapp/checkout.html")
+
+
+
+
+
