60d3c2f29fe7cf874436ad059c0855d07964d9b2
amanbaid99 <amanbaid99@gmail.com>
2020-08-23 16:10:35 +0530

    pulled back

--- foodapp/admin.py
+++ foodapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Customer) 
+admin.site.register(Business)
+admin.site.register(Menu)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
